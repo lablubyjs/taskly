12026551e18efc1502e7b797b47de2cea1ed5512
Crislâine Santos <criis.santos@outlook.com>
2022-08-04 20:42:21 -0300

    feat: add calendar component

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,20 +1,168 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
+import {DateMatrix, createDateMatrix} from 'date-matrix'
+
+import ArrowLeft from '../../../public/images/arrow-left.svg'
+import ArrowRight from '../../../public/images/arrow-right.svg'
+
+import {Button} from '../Button'
+
 import * as S from './styles'
+import {lightTheme, Text} from '@/styles'
 
 interface Calendar {
-  month: string
+  month: number
   year: number
-  mode: number
+  minWeek: number
+  maxWeek: number
+  increment: number
+  dateMatrix: DateMatrix
+}
+
+const changeIncrement = (value: number, state: Calendar): Calendar => {
+  return {
+    ...state,
+    minWeek: 0,
+    maxWeek: value,
+    increment: value,
+  }
+}
+
+const incrementWeek = (state: Calendar): Calendar => {
+  if (state.maxWeek + state.increment > state.dateMatrix.weeks.length) {
+    const newMonth = state.month < 11 ? state.month + 1 : 0
+    const newYear = state.month === 11 ? state.year + 1 : state.year
+
+    return {
+      month: newMonth,
+      year: newYear,
+      minWeek: 0,
+      maxWeek: state.increment,
+      increment: state.increment,
+      dateMatrix: createDateMatrix(new Date(newYear, newMonth, 1)),
+    }
+  }
+
+  return {
+    ...state,
+    minWeek: state.minWeek + state.increment,
+    maxWeek: state.maxWeek + state.increment,
+  }
+}
+
+const decrementWeek = (state: Calendar): Calendar => {
+  if (state.minWeek - state.increment < 0) {
+    const newMonth = state.month > 0 ? state.month - 1 : 11
+    const newYear = state.month === 0 ? state.year - 1 : state.year
+    const newDateMatrix = createDateMatrix(new Date(newYear, newMonth, 1))
+    const monthLength = newDateMatrix.weeks.length
+
+    return {
+        month: newMonth,
+        year: newYear,
+        minWeek: monthLength - state.increment,
+        maxWeek: monthLength,
+        increment: state.increment,
+        dateMatrix: newDateMatrix,
+    }
+  }
+
+  return {
+    ...state,
+    minWeek: state.minWeek - state.increment,
+    maxWeek: state.maxWeek - state.increment,
+  }
 }
 
 export const Calendar = () => {
   const [calendarState, setCalendarState] = useState<Calendar>({
-    month: new Date().toLocaleString('default', { month: 'long' }),
+    month: new Date().getMonth(),
     year: new Date().getFullYear(),
-    mode: 2
+    minWeek: 0,
+    maxWeek: 2,
+    increment: 2,
+    dateMatrix: createDateMatrix(new Date()),
   })
 
+  const weeks = calendarState.dateMatrix.weeks.slice(
+    calendarState.minWeek,
+    calendarState.maxWeek
+  )
+  const monthName = new Date(
+    calendarState.year,
+    calendarState.month,
+    1
+  ).toLocaleDateString('default', {month: 'long'})
+
   return (
-    <S.Title>{calendarState.month}, {calendarState.year}</S.Title>
+    <S.CalendarContainer>
+      <S.CalendarHeader>
+        <Text fontSize={1.5} fontWeight="regular">{monthName}, {calendarState.year}</Text>
+          <S.ButtonsContainer>
+            <Button
+              backgroundColor={lightTheme.buttonControl}
+              height={1.25}
+              width={1.25}
+              borderRadius={50}
+              onClick={() =>
+                  setCalendarState(decrementWeek(calendarState))
+              }
+            >
+              <ArrowLeft />
+            </Button>
+            <Button
+              backgroundColor={lightTheme.buttonControl}
+              height={1.25}
+              width={1.25}
+              borderRadius={50}
+              onClick={() =>
+                  setCalendarState(incrementWeek(calendarState))
+              }
+            >
+              <ArrowRight />
+            </Button>
+          </S.ButtonsContainer>
+          <S.SelectMode
+            defaultValue={2}
+            onChange={(e) =>
+              setCalendarState(
+                changeIncrement(+e.target.value, calendarState)
+              )
+            }
+          >
+            <option value={1}>Uma Semana</option>
+            <option value={2}>Duas Semanas</option>
+            <option value={3}>Três Semanas</option>
+            <option value={5}>Mês</option>
+          </S.SelectMode>
+      </S.CalendarHeader>
+      <S.CalendarBody>
+        <thead>
+            <tr>
+                {calendarState.dateMatrix.weekdays.map((day) => (
+                    <th key={day}>{day}</th>
+                ))}
+            </tr>
+        </thead>
+        <tbody>
+          {weeks.map((week, index) => (
+            <tr key={index}>
+              {week.map((day) => {
+                if (
+                  day.date.toString() ===
+                  new Date().toString()
+                ) {
+                    return (
+                      <td className="today" key={day.day}>
+                        {day.day}
+                      </td>
+                    )
+                }
+                return <td key={day.day}>{day.day}</td>
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </S.CalendarBody>
+    </S.CalendarContainer>
   )
 }
--- src/components/Calendar/styles.ts
+++ src/components/Calendar/styles.ts
@@ -1,8 +1,81 @@
 import styled from 'styled-components'
 
-export const Title = styled.p`
-  background-color: ${({theme}) => theme.panelLeft};
-  color: ${({theme}) => theme.textDark};
+export const CalendarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+`
+
+export const CalendarHeader = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  gap: 1rem;
   text-transform: capitalize;
-  font-size: 1.5rem;
+
+  p {
+    flex: 3;
+  }
+
+  select {
+    flex: 2;
+  }
+
+  div {
+    flex: 1;
+  }
+`
+
+export const CalendarBody = styled.table`
+  padding: 1rem;
+  border-radius: 20px;
+  background-color: ${({theme}) => theme.background};
+  text-align: center;
+  table-layout: fixed;
+
+  th {
+    color: ${({theme}) => theme.textLight};
+    text-transform: capitalize;
+  }
+
+  td {
+    color: ${({theme}) => theme.textNumber};
+    text-transform: capitalize;
+  }
+
+  td, th {
+    font-weight: 600;
+    width: 2.5rem;
+    height: 2.5rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  
+  .today, .taskDay {
+    color: ${({theme}) => theme.textDark};
+  }
+
+  .today {
+    background-color: ${({theme}) => theme.buttonDone};
+    border-radius: 50%;
+  }
+`
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  gap: 0.25rem;
+  margin-top: 0.25rem;
+`
+
+export const SelectMode = styled.select`
+  width: 100%;
+  text-decoration: none;
+  border: none;
+  background: transparent;
+  font-weight: bold;
+  font-family: inherit;
+  font-size: 0.7rem;
+  text-align: center;
+  color: ${({theme}) => theme.textLight};
 `
