aa8eba27626dc40d9d47c27f861199da5132860e
Crislâine Santos <criis.santos@outlook.com>
2022-08-05 15:06:38 -0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/__tests__'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
