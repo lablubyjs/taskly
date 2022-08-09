7fe00b0b6691e1f8db7ffa8cd80095c472d98868
Crislâine Santos <criis.santos@outlook.com>
2022-08-09 17:46:04 -0300

    feat: add axios config for requests on server side

---
+++ src/shared/services/axios.client.config.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosError } from 'axios'
+import { parseCookies } from 'nookies'
+
+export const getApiClient = (context: any) => {
+  const instance = axios.create({
+    baseURL: 'http://localhost:3000/api',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  instance.interceptors.response.use(
+    async (response) => {
+      return response.data
+    },
+
+    (error: AxiosError) => {
+      return Promise.reject(error)
+    }
+  )
+
+  instance.interceptors.request.use(
+    async (config) => {
+      const { accessToken } = parseCookies(context)
+
+      if (accessToken) {
+        config.headers!.Authorization = `Bearer ${accessToken}`
+      }
+
+      return config
+    },
+
+    (error: AxiosError) => {
+      return Promise.reject(error)
+    }
+  )
+
+  return instance
+}
