71fbfe83f7a6e036e6f539f8961ca93adf5ec118
Crislâine Santos <criis.santos@outlook.com>
2022-08-08 21:41:09 -0300

    style: add Text, FlexRowContainer and FlexColumnContainer styled components

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,2 +1,27 @@
 import styled from 'styled-components'
 
+interface TextProps {
+  fontSize: number;
+  fontWeight: string;
+  color?: string;
+}
+
+export const Text = styled.p<TextProps>`
+  font-size: ${({fontSize}) => `${fontSize}rem`};
+  font-weight: ${({fontWeight}) => fontWeight ? fontWeight : 'regular'};
+  color: ${({color, theme}) => color ? color : theme.textDark};
+`
+
+export const FlexRowContainer = styled.div`
+  display: flex;
+  gap: 0.8rem;
+`
+
+export const FlexColumnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+`
--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,10 +1,12 @@
 export interface Theme {
   background: string
-  panelLeft: string
+  sideMenu: string
   buttonPinned: string
   buttonDone: string
   buttonTask: string
+  buttonControl: string
   tagBackground: string
+  textNumber: string
   textDark: string
   textLight: string
   stroke: string
@@ -12,11 +14,13 @@ export interface Theme {
 
 export const lightTheme: Theme = {
   background: '#FFFFFF',
-  panelLeft: '#F6F7FB',
+  sideMenu: '#F6F7FB',
   buttonPinned: '#FFFFFF',
   buttonDone: '#F8D57E',
   buttonTask: '#F6F7FB',
+  buttonControl: '#ECEFF2',
   tagBackground: '#F4B860',
+  textNumber: '#667180',
   textDark: '#2C2543',
   textLight: '#F4B860',
   stroke: '#979797'
@@ -24,11 +28,13 @@ export const lightTheme: Theme = {
 
 export const darkTheme: Theme = {
   background: '#525050',
-  panelLeft: '#363636',
+  sideMenu: '#363636',
   buttonPinned: '#999A9E',
   buttonDone: '#F4B860',
   buttonTask: '#363636',
+  buttonControl: '#FFFFFF',
   tagBackground: '#CF8F32',
+  textNumber: '#667180',
   textDark: '#2C2543',
   textLight: '#FFFFFF',
   stroke: '#979797'
