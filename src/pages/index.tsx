15dd9744824dffa5db42371ef3173559794b7b1b
Crislâine Santos <criis.santos@outlook.com>
2022-08-09 17:53:50 -0300

    feat: add getServerSideProps function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,46 @@
+import { parseCookies } from 'nookies'
 import * as C from '@/components'
 
-import type {NextPage} from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import { IUser } from '@/shared/interfaces'
+import { getApiClient } from '@/shared/services/axios.client.config'
 
-const Home: NextPage = () => {
-    return (
-        <main>
-            <C.SideMenu />
-        </main>
-    );
-};
+const Home: NextPage = (props) => {
+  console.log(props)
+
+  return (
+    <main>
+      <C.SideMenu />
+    </main>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { accessToken } = parseCookies(context)
+  const apiClient = getApiClient(context)
+
+  if (!accessToken) {
+    return {
+      redirect: {
+        destination: 'authentication/login',
+        permanent: false,
+      },
+    }
+  }
+
+  const user = await apiClient.get('/user/account')
+
+  return {
+    props: {
+      user,
+    },
+  }
+}
+
+namespace Home {
+  export type Props = {
+    user: IUser
+  }
+}
 
 export default Home
