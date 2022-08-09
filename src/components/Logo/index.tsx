fdb2c0fd13c4a72a70b4eb4bda6659a880e276ee
Crislâine Santos <criis.santos@outlook.com>
2022-08-08 21:42:15 -0300

    feat: add Logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,19 @@
+import { FlexRowContainer, lightTheme, Text } from '@/styles';
+import React from 'react'
+import { Button } from '../Button';
+import Plus from '../../../public/images/plus.svg'
+
+export const Logo = () => {
+  return (
+    <FlexRowContainer>
+      <Button 
+        backgroundColor={lightTheme.buttonDone}
+        height={1.9}
+        width={1.8}
+        borderRadius={'15px 10px 13px 12px'}
+        onClick={() => {}}
+      ><Plus/></Button>
+      <Text fontSize={1.25} fontWeight='700' color={lightTheme.textDark} >taskly</Text>
+    </FlexRowContainer>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './Calendar'
 export * from './SideMenu'
+export * from './Button'
+export * from './Logo'
