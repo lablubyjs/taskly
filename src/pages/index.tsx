510ab1856c5467adabeb0ce8629c1d281cd9cc26
Crislâine Santos <criis.santos@outlook.com>
2022-08-10 22:40:14 -0300

    feat: add wrapper get server side props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
+import type { NextPage } from 'next'
 import { parseCookies } from 'nookies'
+
 import * as C from '@/components'
 
-import type { GetServerSideProps, NextPage } from 'next'
-import { IUser } from '@/shared/interfaces'
-import { getApiClient } from '@/shared/services/axios.client.config'
+import { ITask, IUser } from '@/shared/interfaces'
+import { getApiClient } from '@/shared/services'
 
-const Home: NextPage = (props) => {
-  console.log(props)
+import { wrapper } from '@/store'
+import { addUser, addTasks } from '@/store/slices'
 
+const Home: NextPage<Home.Props> = (props) => {
   return (
     <main>
       <C.SideMenu />
@@ -15,31 +17,38 @@ const Home: NextPage = (props) => {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { accessToken } = parseCookies(context)
-  const apiClient = getApiClient(context)
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async (context) => {
+    const { accessToken } = parseCookies(context)
+    const apiClient = getApiClient(context)
 
-  if (!accessToken) {
-    return {
-      redirect: {
-        destination: 'authentication/login',
-        permanent: false,
-      },
+    if (!accessToken) {
+      return {
+        redirect: {
+          destination: 'authentication/login',
+          permanent: false,
+        },
+      }
     }
-  }
 
-  const user = await apiClient.get('/user/account')
+    const user: IUser = await apiClient.get('/user/account')
+    const tasks: ITask[] = await apiClient.get('/tasks/list')
 
-  return {
-    props: {
-      user,
-    },
-  }
-}
+    store.dispatch(addUser(user))
+    store.dispatch(addTasks(tasks))
+
+    return {
+      props: {
+        user,
+        tasks
+      }
+    }
+})
 
 namespace Home {
   export type Props = {
     user: IUser
+    tasks: ITask[]
   }
 }
 
