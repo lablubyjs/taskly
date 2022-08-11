028fca6912196571587a932bd1181f3b856228b4
Crislâine Santos <criis.santos@outlook.com>
2022-08-11 17:38:59 -0300

    style: add empty task and list tasks styled components

--- src/components/Tasks/styles.ts
+++ src/components/Tasks/styles.ts
@@ -15,9 +15,24 @@ export const PinnedTasksContainer = styled.div`
   flex: 1;
 `
 
-export const TaskContainer = styled.div`
+export const TaskPinnedContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 3fr;
+`
+
+export const TaskListContainer = styled.div`
+  width: 100%;
+  justify-content: space-between;
+  display: flex;
+  flex-wrap: wrap;
+
+  p:nth-child(2) {
+    width: 70%;
+  }
+
+  div:last-child {
+    padding-left: 3rem;
+  }
 `
 
 export const TaskEmoji = styled.div<TaskEmojiProps>`
@@ -46,4 +61,46 @@ export const TaskTag = styled.p`
   font-size: 0.7rem;
   font-weight: 700;
   color: ${({ theme }) => theme.background}; ;
+`
+export const TasksListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const TasksListHeader = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+`
+
+export const TasksListDateContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-flow: row wrap;
+  gap: 1rem;
+  align-items: flex-end;
+  align-content: space-between;
+
+  p:first-child {
+    width: 100%;
+  }
+`
+export const TasksList = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 1rem;
+`
+
+export const EmptyTasksListContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  width: 50%;
 `
