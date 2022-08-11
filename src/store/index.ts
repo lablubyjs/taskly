6a283562cc32c1921e8676cbaa96d16dec35d505
Crislâine Santos <criis.santos@outlook.com>
2022-08-10 22:35:10 -0300

    feat: add combinedReducer and masterReducer functions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,37 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-import themeModeSlice from './theme-slice'
-import userSlice from './user-slice'
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit'
+import { HYDRATE, createWrapper } from 'next-redux-wrapper'
+import { user, tasks, themeMode } from '@/store/slices'
+
+const combinedReducer = combineReducers({
+  user,
+  tasks,
+  themeMode
+})
+
+const masterReducer = (state: any, action: any) => {
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state,
+      user: {
+        user: action.payload.user.user,
+        isAuthenticated: true
+      },
+      tasks: {
+        list: action.payload.tasks.list.tasks,
+        pinnedTasks: action.payload.tasks.list.tasks
+      },
+      themeMode: action.payload.themeMode || state.themeMode
+    }
+    return nextState
+  } 
+  return combinedReducer(state, action)
+}
 
 export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      user: userSlice,
-      themeMode: themeModeSlice,
-    }
-  })
+  return configureStore({ reducer: masterReducer })
 }
+
+export const wrapper = createWrapper(makeStore, { debug: true })
 
 const store = makeStore()
 
