0f1b6d80ed3d8bf8b9569b9d75c912bda505953c
Crislâine Santos <criis.santos@outlook.com>
2022-08-12 18:45:38 -0300

    feat: add settings page

---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,123 @@
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+import * as C from '@/components'
+
+import { useAppDispatch, useAppSelector } from '@/hooks'
+
+import {
+  selectSettings,
+  selectSettingsTheme,
+  setSettings,
+} from '@/store/slices'
+
+import { darkTheme, FlexColumnContainer, lightTheme, Text } from '@/styles'
+
+const settingsSchema = yup.object({
+  themeMode: yup.string().required('Please enter the theme mode'),
+  pomodoroTimer: yup.string().required('Please enter the pomodoro timer'),
+})
+
+type SettingsValues = {
+  themeMode: string
+  pomodoroTimer: string
+}
+
+const Settings: NextPage = () => {
+  const router = useRouter()
+  const dispatch = useAppDispatch()
+  const settings = useAppSelector(selectSettings)
+  const theme = useAppSelector(selectSettingsTheme)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SettingsValues>({
+    resolver: yupResolver(settingsSchema),
+  })
+
+  const handleSettings = ({ themeMode, pomodoroTimer }: SettingsValues) => {
+    const theme = themeMode === 'light' ? lightTheme : darkTheme
+
+    const pomodoroTimerNumber = +pomodoroTimer * 3600
+    dispatch(
+      setSettings({ themeMode, theme, pomodoroTimer: pomodoroTimerNumber })
+    )
+  }
+
+  return (
+    <main>
+      <FlexColumnContainer>
+        <C.Logo />
+        <C.Form onSubmit={handleSubmit(handleSettings)}>
+          <Text fontSize={2} fontWeight="500" color={theme.textDark}>
+            You settings
+          </Text>
+          <C.Label error={errors.themeMode}>
+            <Text fontSize={1} fontWeight="500" color={theme.textDark}>
+              Theme Mode
+            </Text>
+            <select
+              {...register('themeMode')}
+              defaultValue={settings.themeMode}
+            >
+              <option value="light">Light</option>
+              <option value="dark">Dark</option>
+            </select>
+          </C.Label>
+          <C.Label error={errors.pomodoroTimer}>
+            <Text fontSize={1} fontWeight="500" color={theme.textDark}>
+              Pomodoro Timer
+            </Text>
+            <Text fontSize={0.9} fontWeight="500" color={theme.textLight}>
+              The time is defined in hours
+            </Text>
+            <input
+              placeholder="Timer"
+              {...register('pomodoroTimer')}
+              defaultValue={settings.pomodoroTimer / 3600}
+            />
+          </C.Label>
+          <C.Button
+            width={'25vw'}
+            height={3}
+            backgroundColor={theme.buttonDone}
+            borderRadius={'15px'}
+          >
+            <Text fontSize={1} fontWeight="700" color={theme.textDark}>
+              Save settings
+            </Text>
+          </C.Button>
+        </C.Form>
+        <C.Button
+          width={'25vw'}
+          height={3}
+          backgroundColor="transparent"
+          borderRadius={'15px'}
+        >
+          <Text fontSize={1.1} fontWeight="700" color={theme.textLight}>
+            Log out
+          </Text>
+        </C.Button>
+        <C.Button
+          width={'25vw'}
+          height={3}
+          backgroundColor="transparent"
+          borderRadius={'15px'}
+          onClick={() => router.push('/')}
+        >
+          <Text fontSize={1.25} fontWeight="700" color={theme.textLight}>
+            Go back home
+          </Text>
+        </C.Button>
+      </FlexColumnContainer>
+    </main>
+  )
+}
+
+export default Settings
