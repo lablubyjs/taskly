3ab297c9bab42baa467718e1b0aae79ca013128f
Crislâine Santos <criis.santos@outlook.com>
2022-08-12 18:59:52 -0300

    feat: add list tasks page

---
+++ src/pages/tasks/list.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Link from 'next/link'
+
+import * as C from '@/components'
+
+import { useAppSelector } from '@/hooks'
+
+import { selectSettingsTheme } from '@/store/slices'
+
+import { FlexColumnContainer, Text } from '@/styles'
+
+const List: NextPage = () => {
+  const theme = useAppSelector(selectSettingsTheme)
+
+  return (
+    <main>
+      <FlexColumnContainer>
+        <C.Logo />
+        <Link href="/">
+          <Text fontSize={1.25} fontWeight="500" color={theme.textLight}>
+            Go back home
+          </Text>
+        </Link>
+      </FlexColumnContainer>
+    </main>
+  )
+}
+
+export default List
