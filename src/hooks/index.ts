2647843d5ce3157ec5a53da2181fb8e6d12f2cd3
Crislâine Santos <criis.santos@outlook.com>
2022-08-17 16:38:54 -0300

    refactor: organize hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,2 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-
-import type { AppState, AppDispatch } from '@/store'
-
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-
-export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector
+export * from './useAppDispatch'
+export * from './useAppSelector'
